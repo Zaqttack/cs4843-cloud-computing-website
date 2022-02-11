866c4a123c6ca109a872dbccad2ea7e7f650d846
Zaquariah Holland <zaquariah@gmail.com>
2022-02-10 18:40:47 -0600

    corndog background

--- src/App.js
+++ src/App.js
@@ -3,22 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          I've now updated to version2!
-        </h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div id="corndog"></div>
   );
 }
 
